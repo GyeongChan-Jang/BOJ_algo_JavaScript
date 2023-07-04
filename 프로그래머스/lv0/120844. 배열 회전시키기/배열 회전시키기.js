6a254f8e977bf55b2fec404ceaef6e4e49e8d7c4
GyeongChan Jang <cham9994@gmail.com>
2023-07-04 21:59:33 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,26 +1,36 @@
 function solution(numbers, direction) {
     var answer = []
     
-    const last = [...numbers].pop()
-    const first = [...numbers].shift()
+    // 1. 첫번째 값, 마지막 값 미리 구해서 풀기
+//     const last = [...numbers].pop()
+//     const first = [...numbers].shift()
     
-    numbers.map((v, i) => {
-        if(direction === 'right') {
-            if(i === 0) {
-            answer.push(last)
-            } 
-            if(i +1 === numbers.length) return
-            answer.push(v)
-        }
-        else if ( direction === 'left') {
-            if(i === 0) return
-            answer.push(v)
-            if(i+1 === numbers.length ) {
-                answer.push(first)
-            }
+//     numbers.map((v, i) => {
+//         if(direction === 'right') {
+//             if(i === 0) {
+//             answer.push(last)
+//             } 
+//             if(i +1 === numbers.length) return
+//             answer.push(v)
+//         }
+//         else if ( direction === 'left') {
+//             if(i === 0) return
+//             answer.push(v)
+//             if(i+1 === numbers.length ) {
+//                 answer.push(first)
+//             }
             
-        }
-    })
+//         }
+//     })
+    // return answer
+    
+    // 2. 뺀 값을 바로 넣어주는 풀이
+    if( direction === 'right') {
+        numbers.unshift(numbers.pop())
+    } else {
+        numbers.push(numbers.shift())
+    } 
+    return numbers
     
     return answer
 }
