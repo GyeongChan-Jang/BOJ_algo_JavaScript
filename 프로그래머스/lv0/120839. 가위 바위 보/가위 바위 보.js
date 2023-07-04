cb0e26cbfbc5eb355d8ded275f327d2407762eaa
GyeongChan Jang <cham9994@gmail.com>
2023-07-04 21:20:39 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,21 +1,28 @@
 function solution(rsp) {
-    var answer = []
-    
     // 가위 - 2
     // 바위 - 0
     // 보 - 5
     
-    const rspArray = rsp.split('')
+    // 1. if 문으로 풀기
+//     var answer = []
     
-    rspArray.map((v, i) => {
-        if(v==='2') {
-            answer.push(0)
-        } else if(v==='0') {
-            answer.push(5)
-        } else if(v==='5') {
-            answer.push(2)
-        }
-    })    
+//     const rspArray = rsp.split('')
     
-    return answer.join('');
+//     rspArray.map((v, i) => {
+//         if(v==='2') {
+//             answer.push(0)
+//         } else if(v==='0') {
+//             answer.push(5)
+//         } else if(v==='5') {
+//             answer.push(2)
+//         }
+//     })    
+    
+//     return answer.join('');
+    
+    // 2. 객체로 풀기
+    
+    let answer = {'0': '5', '2': '0', '5': '2'}
+    
+    return [...rsp].map((v) => answer[v]).join('')
 }
