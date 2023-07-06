988cdf2aced4ea411f89210aad2ff84a82db1ce7
GyeongChan Jang <cham9994@gmail.com>
2023-07-06 23:27:45 +0900

    [level 0] Title: k의 개수, Time: 5.98 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/README.md
+++ 프로그래머스/lv0/120887. k의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] k의 개수 - 120887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.3 MB, 시간: 34.04 ms
+메모리: 38.4 MB, 시간: 5.98 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,12 +1,22 @@
 function solution(i, j, k) {
-    var answer = 0;
+//     1. 배열을 이용한 내 풀이
+//     var answer = 0;
+    
+//     for(i; i<=j; i++) {
+//         let arr = i.toString().split('')
+//         if(arr.includes(k.toString())) {
+//             answer += arr.filter((v) => v === k.toString()).length
+//         }
+//     }
+    
+//     return answer;
+    
+//     2. 문자열 풀이
+    let s = ''
     
     for(i; i<=j; i++) {
-        let arr = i.toString().split('')
-        if(arr.includes(k.toString())) {
-            answer += arr.filter((v) => v === k.toString()).length
-        }
+        s += i
     }
     
-    return answer;
+    return s.split(k).length -1
 }
