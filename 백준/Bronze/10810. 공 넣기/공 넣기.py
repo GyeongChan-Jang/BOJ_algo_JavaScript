eb12087209b0a77f398ae7459651968c4c0954ed
GyeongChan Jang <cham9994@gmail.com>
2023-10-03 14:37:49 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,9 @@
+totalBasket, throwCount = map(int, input().split())
+basketList = [0 for _ in range(totalBasket)]
+
+
+for _ in range(throwCount):
+    start, end, number = map(int, input().split())
+    for v in range(start-1, end):
+        basketList[v] = number
+print(*basketList)
